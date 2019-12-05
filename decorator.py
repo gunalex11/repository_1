e5e8329c73b4ee22acf0fa96f9289dcc5bde73e5
Lesha <gunalex11@gmail.com>
2019-12-05 23:35:16 +0300

    I nearly started hating decorators, but now it's fine ^_^

--- decorator.py
+++ decorator.py
@@ -13,9 +13,9 @@ def plus_1(x):
 
 
 @repeat(0)
-def mul_2(x):
+def mult_2(x):
     return x * 2
 
 
 print(plus_1(3))  # должно выдать 5
-print(mul_2(4))  # должно выдать 4
+print(mult_2(4))  # должно выдать 4
