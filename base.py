0c7e3d4f6a24d1afdde480f2df47e71d4f34d963
Lesha <gunalex11@gmail.com>
2019-12-09 20:09:11 +0300

    Updates in the task

--- base.py
+++ base.py
@@ -1,14 +1,16 @@
-"""Вывод числа в произвольной системе счисления( <= 36)"""
+"""Вывод числа в произвольной системе счисления(<= 36)"""
 
 def to_str(arg: int) -> str:
     """Переводит целое число из [0, 35] в строку, содержащую символ от 0 до 9 или от A до Z"""
     if arg < 10:
-        return chr(arg + 48)
-    return chr(arg + 55)
+        return chr(arg + ord('0'))
+    return chr(arg + ord('A') - 10)
 
-def str_base(value: 'int', base: 'int') -> 'str':
+def str_base(value: int, base: int) -> str:
     """Shows value using number base, e.g. str_base(44027, 36)=='XYZ'"""
 
+    if base not in range(2, 37):
+        return "Mi scusi, you're mistaken :c"
     output: str = ""
     while value != 0:
         output += to_str(value % base)
@@ -16,9 +18,6 @@ def str_base(value: 'int', base: 'int') -> 'str':
     return ''.join(reversed(output))
 
 V = int(input("Value? (integer)" + '\n'))
-B = int(input("Base? (integer in [1, 36])" + '\n'))
+B = int(input("Base? (integer in [2, 36])" + '\n'))
 
-if B not in range(1, 37):
-    print("Mi scusi, you're mistaken :c")
-else:
-    print(str_base(V, B))
+print(str_base(V, B))
